cc82d55b3052017ae92af97e214b9bf7e569ea1b
津口雄作 <tsuguchi@nnn.or.jp>
2025-07-12 17:04:14 +0900

    resource の削除

--- tests.tf
+++ tests.tf
@@ -1,70 +1,58 @@
 check "alb_configuration" {
-  resource "aws_lb" "target" {
-    name = aws_lb.raise_tech_alb.name
+
+  assert {
+    condition     = aws_lb.raise_tech_alb.internal == false
+    error_message = "ALB '${aws_lb.raise_tech_alb.name}' は internal=true になっています。このALBはパブリックである必要があります。"
   }
 
   assert {
-    condition     = self.internal == false
-    error_message = "ALB '${self.name}' は internal=true になっています。このALBはパブリックである必要があります。"
+    condition     = aws_lb.raise_tech_alb.load_balancer_type == "application"
+    error_message = "ALB '${aws_lb.raise_tech_alb.name}' のタイプが '${aws_lb.raise_tech_alb.load_balancer_type}' です。'application' である必要があります。"
   }
 
   assert {
-    condition     = self.load_balancer_type == "application"
-    error_message = "ALB '${self.name}' のタイプが '${self.load_balancer_type}' です。'application' である必要があります。"
+    condition     = length(aws_lb.raise_tech_alb.subnets) >= 2
+    error_message = "ALB '${aws_lb.raise_tech_alb.name}' は ${length(aws_lb.raise_tech_alb.subnets)} 個のサブネットにしか配置されていません。高可用性のために2つ以上のサブネットに配置してください。"
   }
 
   assert {
-    condition     = length(self.subnets) >= 2
-    error_message = "ALB '${self.name}' は ${length(self.subnets)} 個のサブネットにしか配置されていません。高可用性のために2つ以上のサブネットに配置してください。"
-  }
-
-  assert {
-    condition     = length(self.security_groups) > 0
-    error_message = "ALB '${self.name}' にセキュリティグループが設定されていません。"
+    condition     = length(aws_lb.raise_tech_alb.security_groups) > 0
+    error_message = "ALB '${aws_lb.raise_tech_alb.name}' にセキュリティグループが設定されていません。"
   }
 }
 
 check "target_group_configuration" {
 
-  resource "aws_lb_target_group" "target" {
-    name = aws_lb_target_group.alb_tg.name
+  assert {
+    condition     = aws_lb_target_group.alb_tg.protocol == "HTTP"
+    error_message = "ターゲットグループ '${aws_lb_target_group.alb_tg.name}' のプロトコルが '${aws_lb_target_group.alb_tg.protocol}' です。'HTTP' である必要があります。"
   }
 
   assert {
-    condition     = self.protocol == "HTTP"
-    error_message = "ターゲットグループ '${self.name}' のプロトコルが '${self.protocol}' です。'HTTP' である必要があります。"
+    condition     = aws_lb_target_group.alb_tg.port == 80
+    error_message = "ターゲットグループ '${aws_lb_target_group.alb_tg.name}' のポートが ${aws_lb_target_group.alb_tg.port} です。ポート80である必要があります。"
   }
 
   assert {
-    condition     = self.port == 80
-    error_message = "ターゲットグループ '${self.name}' のポートが ${self.port} です。ポート80である必要があります。"
-  }
-
-  assert {
-    condition     = self.vpc_id == aws_vpc.raise_tech.id
-    error_message = "ターゲットグループ '${self.name}' が意図しないVPC '${self.vpc_id}' に関連付けられています。"
+    condition     = aws_lb_target_group.alb_tg.vpc_id == aws_vpc.raise_tech.id
+    error_message = "ターゲットグループ '${aws_lb_target_group.alb_tg.name}' が意図しないVPC '${aws_lb_target_group.alb_tg.vpc_id}' に関連付けられています。"
   }
 }
 
 check "listener_configuration" {
 
-  resource "aws_lb_listener" "target" {
-    load_balancer_arn = aws_lb_listener.alb_listener.load_balancer_arn
-    port              = aws_lb_listener.alb_listener.port
+  assert {
+    condition     = aws_lb_listener.alb_listener.protocol == "HTTP"
+    error_message = "リスナーのプロトコルが '${aws_lb_listener.alb_listener.protocol}' です。'HTTP' である必要があります。"
   }
 
   assert {
-    condition     = self.protocol == "HTTP"
-    error_message = "リスナーのプロトコルが '${self.protocol}' です。'HTTP' である必要があります。"
+    condition     = aws_lb_listener.alb_listener.port == 80
+    error_message = "リスナーのポートが ${aws_lb_listener.alb_listener.port} です。ポート80である必要があります。"
   }
 
   assert {
-    condition     = self.port == 80
-    error_message = "リスナーのポートが ${self.port} です。ポート80である必要があります。"
-  }
-
-  assert {
-    condition     = self.default_action[0].target_group_arn == aws_lb_target_group.alb_tg.arn
+    condition     = aws_lb_listener.alb_listener.default_action[0].target_group_arn == aws_lb_target_group.alb_tg.arn
     error_message = "リスナーのデフォルトアクションが、意図しないターゲットグループを向いています。"
   }
 }
